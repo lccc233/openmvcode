8f1d9fd017aeb4b0221d261a2fc7525850f29bd5
LIC <meecli@nuaa.edu.cn>
2021-07-29 21:00:12 +0800

    fix uart bug

--- 视觉测试版.py
+++ 视觉测试版.py
@@ -12,13 +12,14 @@ x_mid=160
 y_mid=120
 center=-1
 QRresult=False
+
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.skip_frames(10)
+sensor.skip_frames(2000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
-clock = time.clock()
+clock = time.clock()#
 
 uart1=UART(1,115200)
 uart3=UART(3,115200)
@@ -28,7 +29,7 @@ while(True):
     x_p=-1
     y_p=-1
     center=-1
-    clock.tick()
+    clock.tick()#
     img = sensor.snapshot()
     aim_blobs=img.find_blobs([aim_threshold],x_stride=2,y_stride=2,
                              area_threshold=400,pixel_threshold=100,
@@ -47,7 +48,7 @@ while(True):
             img.binary([aim_threshold])
             img.erode(2)
             img.dilate(3)
-            img.draw_rectangle(blob.rect(),(0,255,255))
+            img.draw_rectangle(blob.rect(),(0,255,255))#
             statis=img.get_statistics(roi=(x_p-25,y_p-25,50,50))
             if statis.l_mean()>50:
                 typ=1
@@ -59,9 +60,9 @@ while(True):
             y_p=blob.cy()
             center=abs(x_p-x_mid)+abs(y_p-y_mid)
             typ=3
-            img.draw_rectangle(blob.rect(),(255,0,0))
+            img.draw_rectangle(blob.rect(),(255,0,0))#
     for blob in white_blobs:
-        img.draw_rectangle(blob.rect(),(255,255,255))
+        img.draw_rectangle(blob.rect(),(255,255,255))#
         if(typ==-1 or abs(blob.cx()-x_mid)+abs(blob.cy()-y_mid)<center):
             result=False
             if(uart3.any()):
@@ -73,7 +74,8 @@ while(True):
                 x_p=blob.cx()
                 y_p=blob.cy()
                 center=abs(x_p-x_mid)+abs(y_p-y_mid)
-    data=bytearray([0xee,0xee,0xee,int(typ),int(x_p),int(y_p)])
-    uart1.write(data)
-    print(data)
-    print("FPS %f"%clock.fps())
+    if(typ!=-1 and x_p!=-1 and y_p!=-1):
+        data=bytearray([0xff,0xfe,int(typ),int(x_p/2),int(y_p/2)])
+        uart1.write(data)
+        print(data)
+    print("FPS %f"%clock.fps())#
