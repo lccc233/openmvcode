01d7a7cc4267a474e9ea80b21d4781cc4d9266c2
LIC <meecli@nuaa.edu.cn>
2021-07-29 17:18:00 +0800

    find center first

--- 视觉测试版.py
+++ 视觉测试版.py
@@ -1,4 +1,4 @@
-import sensor, time, image
+import sensor,time,image,math
 from pyb import UART
 
 aim_threshold=()
@@ -8,6 +8,9 @@ typ=-1
 x_p=-1
 y_p=-1
 aim_QRCode='R\r\n'
+x_mid=160
+y_mid=120
+center=-1
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
@@ -24,6 +27,7 @@ while(True):
     typ=-1
     x_p=-1
     y_p=-1
+    center=-1
     clock.tick()
     img = sensor.snapshot()
     aim_blobs=img.find_blobs([aim_threshold],x_stride=2,y_stride=2,
@@ -36,29 +40,35 @@ while(True):
                                area_threshold=200,pixel_threshold=100,
                                merge=True,margin=2)
     for blob in aim_blobs:
-        x_p=blob.cx()
-        y_p=blob.cy()
-        img.binary([aim_threshold])
-        img.erode(2)
-        img.dilate(3)
-        img.draw_rectangle(blob.rect(),(0,255,255))
-        statis=img.get_statistics(roi=(x_p-25,y_p-25,50,50))
-        if statis.l_mean()>50:
-            typ=1
-        else:
-            typ=2
+        if(typ==-1 or abs(blob.cx()-x_mid)+abs(blob.cy()-y_mid)<center):
+            x_p=blob.cx()
+            y_p=blob.cy()
+            center=abs(x_p-x_mid)+abs(y_p-y_mid)
+            img.binary([aim_threshold])
+            img.erode(2)
+            img.dilate(3)
+            img.draw_rectangle(blob.rect(),(0,255,255))
+            statis=img.get_statistics(roi=(x_p-25,y_p-25,50,50))
+            if statis.l_mean()>50:
+                typ=1
+            else:
+                typ=2
     for blob in yelow_blobs:
-        x_p=blob.cx()
-        y_p=blob.cy()
-        typ=3
-        img.draw_rectangle(blob.rect(),(255,0,0))
+        if(typ==-1 or abs(blob.cx()-x_mid)+abs(blob.cy()-y_mid)<center):
+            x_p=blob.cx()
+            y_p=blob.cy()
+            center=abs(x_p-x_mid)+abs(y_p-y_mid)
+            typ=3
+            img.draw_rectangle(blob.rect(),(255,0,0))
     for blob in white_blobs:
-        if(uart3.any()):
-            QRCode = uart3.readline().decode()
-            if(QRCode==aim_QRCode):typ=1
-        x_p=blob.cx()
-        y_p=blob.cy()
-        img.draw_rectangle(blob.rect(),(255,255,255))
+        if(typ==-1 or abs(blob.cx()-x_mid)+abs(blob.cy()-y_mid)<center):
+            if(uart3.any()):
+                QRCode = uart3.readline().decode()
+                if(QRCode==aim_QRCode):typ=1
+            x_p=blob.cx()
+            y_p=blob.cy()
+            center=abs(x_p-x_mid)+abs(y_p-y_mid)
+            img.draw_rectangle(blob.rect(),(255,255,255))
     data=bytearray([0xee,0xee,0xee,int(typ),int(x_p),int(y_p)])
     uart1.write(data)
     print(data)
