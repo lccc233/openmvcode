4ede0ff75553f4b8325374fbe26759128484b2bf
LIC <meecli@nuaa.edu.cn>
2021-08-01 13:18:05 +0800

    pwm

--- servo.py
+++ servo.py
@@ -1,19 +1,20 @@
 from pyb import Timer
-import sensor,pyb
+import sensor,pyb,time
 tim = pyb.Timer(4)
-tim.init(freq=2000)
-p = pyb.LED(1)
+tim.init(freq=4000)
+p = pyb.Pin("P0", pyb.Pin.OUT_PP)
 counter_num=0
+aim_counter=1
 def tick(void):
     global counter_num
     counter_num=counter_num+1
-    if counter_num==40:
+    if counter_num==80:
         counter_num=0
-    if counter_num<=1:
-        p.on()
+    if counter_num<=aim_counter:
+        p.high()
         print(1)
     else:
-        p.off()
+        p.low()
         print(0)
 tim.callback(tick)
 sensor.reset()
@@ -24,3 +25,5 @@ sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
 while True:
     sensor.snapshot()
+    time.sleep_ms(1000)
+
