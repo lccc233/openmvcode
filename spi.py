3dfb99065f00142d87734271bc93782a4480627f
LIC <meecli@nuaa.edu.cn>
2021-09-07 19:29:55 +0800

    ??

--- spi.py
+++ spi.py
@@ -1,20 +1,20 @@
 from pyb import SPI,Pin
 import sensor,pyb,time
 spi = SPI(2, SPI.MASTER, baudrate=3815,polarity=0,phase=0,firstbit=SPI.MSB,bits=8,ti=False,crc=None)
-cs  = Pin("P3", Pin.OUT_OD)
+cs  = Pin("P7", Pin.OUT_OD)
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 data=bytearray(3)
-
+cs.high()
 while (True):
     data1=bytearray(b'abc')
     img = sensor.snapshot()
     cs.low()
     time.sleep_us(20)
-    spi.send(data1,timeout=50)
+    data=spi.send_recv(data1,timeout=50)
     cs.high()
-    spi.recv(data,timeout=50)
+    #spi.recv(data,timeout=50)
     print(data)
     time.sleep_ms(10)
